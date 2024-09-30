d8d1cbe4989199ae53b53280289dd669e7622ef2
Keerthi-Jangam <keerthi.jangam@everest.engineering>
2024-09-30 12:03:06 +0530

    Check for a adding a new saving goal

---
+++ src/tests/SavingGoals.test.ts
@@ -0,0 +1,31 @@
+import { UserManager } from "../models/UserManager";
+import { SavingsManager } from "../models/SavingGoals";
+
+
+describe('Savings Goals Management', () => {
+  let userManager: UserManager;
+  let savingsManager: SavingsManager;
+
+  beforeEach(() => {
+    userManager = new UserManager();
+    const user = {
+      name: "keerthi",
+      income: 20000,
+      categories: [],
+      savings: [
+        { title: "Emergency", target: 10000, current: 8000 },
+        { title: "Vacation", target: 15000, current: 0 },
+      ],
+      otherIncome: 5000,
+      totalIncome: 30000,
+      otherSavings: 5000,
+    };
+    userManager.addUser(user);
+    savingsManager = new SavingsManager(user);
+  });
+
+  it('should add a new savings goal', () => {
+    savingsManager.addSavings('New Goal', 20000);
+    expect(userManager.getUser('keerthi')?.savings.length).toBe(3);
+  });
+})
