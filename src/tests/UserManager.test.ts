21d8731d0ed34761d21b4146c15caab64fca62f3
Keerthi-Jangam <keerthi.jangam@everest.engineering>
2024-09-30 14:15:00 +0530

    Add static data in UserManager test file

--- src/tests/UserManager.test.ts
+++ src/tests/UserManager.test.ts
@@ -1,4 +1,6 @@
 import { UserManager,User } from "../models/UserManager";
+import { data } from "../static_data/data";
+
 
 describe('User Management', () => {
   let userManager: UserManager;
@@ -8,24 +10,7 @@ describe('User Management', () => {
   });
 
   it('should add a new user', () => {
-    const user: User = {
-      name: "keerthi",
-      income: 20000,
-      categories: [
-        { title: "Food", budgetAmount: 8000, spentTillNow: 5000 },
-        { title: "Entertainment", budgetAmount: 2000, spentTillNow: 500 },
-        { title: "Insurance", budgetAmount: 1000, spentTillNow: 500 },
-        { title: "Daily Expenditure", budgetAmount: 4000, spentTillNow: 2000 },
-        { title: "EMI", budgetAmount: 10000, spentTillNow: 2000 },
-      ],
-      savings: [
-        { title: "Emergency", target: 10000, current: 8000 },
-        { title: "Vacation", target: 15000, current: 0 },
-      ],
-      otherIncome: 5000,
-      totalIncome: 30000,
-      otherSavings: 5000,
-    };
+    const user: User = data[0]
 
     userManager.addUser(user);
     expect(userManager.getUser("keerthi")).toEqual(user);
