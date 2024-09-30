13b5f875220e4229279f38a5cb16fa6997a1e8e4
Keerthi-Jangam <keerthi.jangam@everest.engineering>
2024-09-30 12:27:26 +0530

    update add Transaction function

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -9,9 +9,9 @@ export interface Transaction {
     private transactions: Transaction[] = [];
   
     addTransaction(description: string, amount: number, type: 'income' | 'expense'): Transaction {
-      const transaction: Transaction = { description, amount, date: new Date(), type };
-      this.transactions.push(transaction);
-      return transaction;
+        const transaction: Transaction = { description, amount, date: new Date(), type };
+        this.transactions.push(transaction);
+        return transaction;
     }
   
    
