8ed6dc9b45c4c6d21de47a536a1581a60b96de8b
Keerthi-Jangam <keerthi.jangam@everest.engineering>
2024-10-24 10:42:37 +0530

    Remove unwanted files

--- Backend/Types/User.ts
+++
@@ -1,9 +0,0 @@
-export type user={
-    name: string,
-    password: string,
-    totalIncome:number,
-    otherIncome:number,
-    otherSavings:number,
-    salary:number,
-    balance:number
-}
--- Backend/Types/transaction.ts
+++
@@ -1,8 +0,0 @@
-import { Schema } from "mongoose"
-
-export type transaction = {
-    amount:number;
-    date:Date;
-    type:string,
-    category:string
-}
